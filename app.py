75d83baf706ff634ffcf6b7bf42d42f7d4890dcd
jefflueck <lueckcfd@yahoo.com>
2022-03-21 19:36:15 -0500

    full app, delete needs work, styling

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def home_page():
 @app.route('/list')
 def list_page():
   users = User.query.all()
-  return render_template('/list.html',users=users)
+  return render_template('list.html', users=users)
 
 @app.route('/list', methods=["POST"])
 def add_user_page():
@@ -44,6 +44,55 @@ def add_user_page():
   new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
   db.session.add(new_user)
   db.session.commit()
+
   users = User.query.all()
 
-  return render_template('/list.html', users=users)
+  return render_template('list.html', new_user=new_user, users=users)
+
+@app.route('/details/<int:user_id>')
+def show_details(user_id):
+  user = User.query.get_or_404(user_id)
+  return render_template('details.html', user=user)
+
+@app.route('/edit/<int:user_id>')
+def pull_user(user_id):
+
+  user = User.query.get_or_404(user_id)
+  # first_name = request.form['first_name']
+  # last_name = request.form['last_name']
+  # image_url = request.form['image_url']
+  # image_url = image_url if image_url else None
+
+  # db.session.add_all(user,first_name,last_name,image_url)
+  # db.session.commit()
+
+
+  return render_template('edit.html', user=user)
+
+@app.route('/edit/<int:user_id>', methods=["POST"])
+def update_user(user_id):
+  user = User.query.get_or_404(user_id)
+
+  user.first_name = request.form['first_name']
+  user.last_name = request.form['last_name']
+  user.image_url = request.form['image_url']
+
+
+  db.session.add(user)
+  db.session.commit()
+
+  return redirect('/list')
+
+
+@app.route('/delete/<int:user_id>')
+def delete_user(user_id):
+  user = User.query.get_or_404(user_id)
+
+  return render_template('delete.html', user=user)
+
+
+@app.route('/delete/<int:user_id>', methods=["POST"])
+def confirm_delete(user_id):
+  user = User.query.get_or_404(user_id)
+
+  return redirect('/list')
